94adca0ae1e0b56feeaa180d3ab4021425e06a92
thejezzi <38394992+thejezzi@users.noreply.github.com>
2024-05-01 18:10:42 +0200

    fix: config structure for reading the leptos server macro

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,15 +6,13 @@ return {
       ---@type lspconfig.options
       servers = {
         rust_analyzer = {
-          setup = {
-            settings = {
-              ["rust-analyzer"] = {
-                procMacro = {
-                  ignored = {
-                    leptos_macro = {
-                      "component",
-                      "server",
-                    },
+          settings = {
+            ["rust-analyzer"] = {
+              procMacro = {
+                ignored = {
+                  leptos_macro = {
+                    "component",
+                    "server",
                   },
                 },
               },
