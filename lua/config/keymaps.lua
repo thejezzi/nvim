63c1f8d06088220d96be0f6821d235c0004e24a2
thejezzi <38394992+thejezzi@users.noreply.github.com>
2024-05-04 14:57:06 +0200

    feat: add vim-fugitive and add better keymaps
    add a new plugin called vim-fugitive which makes it easy to work
    with merge conflicts via an awesome 3way split view.
    
    also added some better keymaps to interact with gen.nvim with
    some better descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Maps a key or command to a mode.
+--
+-- @param mode The Vim mode (e.g. "n", "v", etc.)
+-- @param lhs The left-hand side of the mapping (e.g. "j" for the 'j' key)
+-- @param rhs The right-hand side of the mapping (e.g. a function or command to be executed)
+-- @param opts Optional additional options to pass to the `vim.api.nvim_set_keymap` function
 local function map(mode, lhs, rhs, opts)
   local options = { noremap = true }
   if opts then
@@ -10,11 +16,22 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- Maps a key or command to a mode with an optional description.
+--
+-- @param mode The Vim mode (e.g. "n", "v", etc.)
+-- @param lhs The left-hand side of the mapping (e.g. "j" for the 'j' key)
+-- @param rhs The right-hand side of the mapping (e.g. a function or command to be executed)
+-- @param desc An optional description for the mapping
+-- @param opts Any additional options to pass to the `map` function
 local function map_desc(mode, lhs, rhs, desc, opts)
   map(mode, lhs, rhs, opts)
   vim.api.nvim_buf_set_var(0, "mapleader_desc_" .. lhs, desc)
 end
 
+-- Maps multiple key bindings to Vim modes.
+--
+-- @param mappings A table of key binding mappings, where each mapping is a table
+--                 containing mode(s), lhs, rhs, description and options (see map_desc).
 local function map_multiple(mappings)
   for _, mapping in ipairs(mappings) do
     local modeStr, lhs, rhs, desc, opts = unpack(mapping)
@@ -28,16 +45,8 @@ local function map_multiple(mappings)
 end
 
 map_multiple({
-  -- additonal goto keymaps
   { "n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", "LSP Hover" },
   { "t", "<Esc>", "<C-\\><C-n>" },
-  { "n", "<leader>zg", "<cmd>Gen<CR>" },
-  { "n", "<leader>zc", "<cmd>Gen Chat<CR>" },
-  { "v", "<leader>za", ":'<,'>Gen Ask<CR>" },
-  { "v", "<leader>zh", ":'<,'>Gen Change<CR>" },
-  { "v", "<leader>ze", ":'<,'>Gen Enhance_Grammar_Spelling<CR>" },
-  { "v", "<leader>zr", ":'<,'>Gen Review_Code<CR>" },
-  { "v", "<leader>zs", ":'<,'>Gen Summarize<CR>" },
 })
 
 local wk = require("which-key")
@@ -60,12 +69,19 @@ wk.register({
       name = "Diffview",
       d = { "<cmd>DiffviewOpen<CR>", "Open Diffview" },
       c = { "<cmd>DiffviewClose<CR>", "Close Diffview" },
-      r = { "<cmd>DiffviewRefresh<CR>", "Refresh Diffview" },
-      h = { "<cmd>DiffviewFileHistory<CR>", "File History" },
     },
+    r = { "<cmd>DiffviewRefresh<CR>", "Refresh Diffview" },
+    h = { "<cmd>DiffviewFileHistory<CR>", "File History" },
   },
   z = {
-    name = "Ollama",
-    -- e = { "<cmd>Gen Enhance_Grammar_Spelling<CR>", "Enhance Grammar Spelling" },
+    name = "ollama",
+    mode = { "n", "v" },
+    g = { mode = "n", "<cmd>Gen<CR>", "gen" },
+    c = { mode = "n", "<cmd>Gen Chat<CR>", "chat" },
+    a = { mode = "v", ":'<,'>Gen Ask<CR>", "ask with prompt" },
+    h = { mode = "v", ":'<,'>Gen Change<CR>", "change selected text" },
+    e = { mode = "v", ":'<,'>Gen Enhance_Grammar_Spelling<CR>", "enhance grammar" },
+    r = { mode = "v", ":'<,'>Gen Review_Code<CR>", "review code" },
+    s = { mode = "v", ":'<,'>Gen Summarize<CR>", "summarize" },
   },
 }, { prefix = "<leader>" })
