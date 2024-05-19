cc9edc46a1de4671ba1ebf708f40757c234dc93b
thejezzi <florianhauptmann@outlook.de>
2024-05-19 16:55:45 +0200

    update lazyvim to version 4

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,1 +1,140 @@
-return {}
+local bear = [[
+   ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄   
+   █▒▒░░░░░░░░░▒▒█   
+    █░░█░░░░░█░░█    
+ ▄▄  █░░░▀█▀░░░█  ▄▄ 
+█░░█ ▀▄░░░░░░░▄▀ █░░█
+]]
+
+local cat = [[
+ ▄▀▄     ▄▀▄
+ ▄█░░▀▀▀▀▀░░█▄
+ ▄▄  █░░░░░░░░░░░█  ▄▄
+█▄▄█ █░░▀░░┬░░▀░░█ █▄▄█
+]]
+
+local coffee = [[
+ ▄▀ ▄▀
+  ▀  ▀
+█▀▀▀▀▀█▄
+█░░░░░█ █
+▀▄▄▄▄▄▀▀
+]]
+
+local cat_big = [[
+            ⢀⡀        ⢀⣠⣤⡀                     
+           ⢠⣿⣿⣷⡀     ⣴⣿⣿⣿⣿⡄                    
+           ⣾⣿⣿⣿⣿⡄  ⢀⣾⣿⣿⠛⣿⣿⣿⡄                   
+          ⢸⣿⣿⣿⣿⣿⣿⣆⣠⣾⣿⡟⠁ ⠸⣿⣿⣇                   
+          ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢄  ⢠⣿⣿⣿                   
+         ⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣅⢠ ⣿⣿⣿     ⢀⣴⣶⣶⣶⣶⣤⣄⡀     
+      ⢀⣀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⡄    ⠸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀  
+     ⣾⣿⣿⣿⣿⣿⣿⣿⣯⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿      ⠉⠉⠛⠛⠿⣿⣿⣿⣿⣿⡄ 
+⠘⠵⠧⢴⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇          ⠈⢻⣿⣿⣿⣿ 
+⠰⣾⢭⣽⣶⢿⣿⣿⣿⣿⣿⣿⣿⣟⣛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟            ⣼⣿⣿⣿⣿ 
+  ⢰⣯⡴⠟⢿⣿⣿⣿⣿⣿⣿⣭⣛⠿⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟          ⢀⣠⣾⣿⣿⣿⣿⠏ 
+       ⠉⠻⣿⣿⣿⣷⣍⠻⣷⣬⣙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋  
+          ⠙⢿⣿⣿⣷⣬⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋    
+            ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄     
+            ⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧     
+            ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄    
+             ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇    
+            ⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄   
+           ⢀⣿⣿⣿⣿⣿⠋⢙⣿⣿⣿⣿⣿⣿⠿⠿⠿⠟⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀ 
+           ⠸⣿⣿⣿⣿⣏ ⣸⣿⣿⣿⣿⣿⠇       ⠉⢻⣿⣿⣿⣿⡟⠻⢿⣿⣿⣿⣿⡇ 
+            ⢿⣿⣿⣿⣿⢀⣿⣿⣿⣿⣿⡿        ⢀⣿⣿⣿⣿⡿   ⠹⣿⣿⣿⣷ 
+            ⠈⢿⣿⣿⡟⣸⣿⣿⣿⣿⣿⠁        ⣼⣿⣿⣿⣿⠃   ⢸⣿⣿⣿⣿⡄
+              ⠉⠁⣰⣿⣿⣿⣿⣿⠇       ⣠⣾⣿⣿⣿⣿⠏    ⣼⣿⣿⣿⣿⡇
+               ⠰⣿⣿⣿⣿⣿⡟        ⠘⠛⠛⠛⠛⠉     ⠿⢿⣿⣿⠟ 
+                ⠉⠉⠉⠉⠁  
+]]
+
+local house = [[
+                           (   )
+                          (    )
+                           (    )
+                          (    )
+                            )  )
+                           (  (                  /\
+                            (_)                 /  \  /\
+                    ________[_]________      /\/    \/  \
+           /\      /\        ______    \    /   /\/\  /\/\
+          /  \    //_\       \    /\    \  /\/\/    \/    \
+   /\    / /\/\  //___\       \__/  \    \/
+  /  \  /\/    \//_____\       \ |[]|     \
+ /\/\/\/       //_______\       \|__|      \
+/      \      /XXXXXXXXXX\                  \
+        \    /_I_II  I__I_\__________________\
+               I_I|  I__I_____[]_|_[]_____I
+               I_II  I__I_____[]_|_[]_____I
+               I II__I  I     XXXXXXX     I
+            ~~~~~"   "~~~~~~~~~~~~~~~~~~~~~~~~
+]]
+
+return {
+  "nvimdev/dashboard-nvim",
+  opts = function(_, _)
+    local function randomAsciiArt()
+      math.randomseed(os.time())
+      local asciiArts = { bear, cat, coffee, cat_big }
+      local randomIndex = math.random(#asciiArts)
+      return asciiArts[randomIndex], randomIndex
+    end
+
+    local ascii, index = randomAsciiArt()
+    local layout = 8
+
+    if index > 4 then
+      layout = 2
+    end
+
+    ascii = string.rep("\n", layout) .. ascii .. "\n\n"
+
+    local opts = {
+      theme = "doom",
+      hide = {
+        -- this is taken care of by lualine
+        -- enabling this messes up the actual laststatus setting after loading a file
+        statusline = false,
+      },
+      config = {
+        header = vim.split(ascii, "\n"),
+        -- stylua: ignore
+        center = {
+          { action = LazyVim.telescope("files"),                                    desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
+          { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+        },
+        footer = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+        end,
+      },
+    }
+
+    for _, button in ipairs(opts.config.center) do
+      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      button.key_format = "  %s"
+    end
+
+    -- close Lazy and re-open when the dashboard is ready
+    if vim.o.filetype == "lazy" then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "DashboardLoaded",
+        callback = function()
+          require("lazy").show()
+        end,
+      })
+    end
+
+    return opts
+  end,
+}
