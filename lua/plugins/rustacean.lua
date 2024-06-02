4e6da4d505867e8a71d6096e22207d7e93b50d9c
thejezzi <38394992+thejezzi@users.noreply.github.com>
2024-06-02 10:12:26 +0200

    refactor: move rust lsp settings to rustacean config
    Lazyvim extra for rust uses the rustacean plugin which apperently
    implements the rustanalyzer itself. Settings in the lspconfig
    file would install a second rustanalyzer which conflicts with the
    existing one from rustacean.
    
    To use the rustanalyzer with rustacean. It has to be installed via
    rustup:
    
    ```
    rustup component add rust-analyzer
    
    ```

---
+++ lua/plugins/rustacean.lua
@@ -0,0 +1,45 @@
+return {
+  "mrcjkb/rustaceanvim",
+  opts = {
+    server = {
+      on_attach = function(_, bufnr)
+        vim.keymap.set("n", "<leader>cR", function()
+          vim.cmd.RustLsp("codeAction")
+        end, { desc = "Code Action", buffer = bufnr })
+        vim.keymap.set("n", "<leader>dr", function()
+          vim.cmd.RustLsp("debuggables")
+        end, { desc = "Rust Debuggables", buffer = bufnr })
+      end,
+      default_settings = {
+        -- rust-analyzer language server configuration
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+            loadOutDirsFromCheck = true,
+            buildScripts = {
+              enable = true,
+            },
+          },
+          -- Add clippy lints for Rust.
+          checkOnSave = {
+            allFeatures = true,
+            command = "clippy",
+            extraArgs = { "--no-deps" },
+          },
+          procMacro = {
+            enable = true,
+            ignored = {
+              ["async-trait"] = { "async_trait" },
+              ["napi-derive"] = { "napi" },
+              ["async-recursion"] = { "async_recursion" },
+              leptos_macro = {
+                "component",
+                "server",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
