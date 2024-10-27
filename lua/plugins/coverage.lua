9cea02870da456d012ad5e671d38c2813d762c45
thejezzi <38394992+thejezzi@users.noreply.github.com>
2024-10-28 00:21:04 +0100

    feat: use fork of nvim-coverage to use lcov files in rust

--- lua/plugins/coverage.lua
+++ lua/plugins/coverage.lua
@@ -1,16 +1,31 @@
 return {
   {
-    "andythigpen/nvim-coverage",
+    "thejezzi/nvim-coverage",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
     init = function()
-      require("coverage").setup()
+      require("coverage").setup({
+        lang = {
+          rust = {
+            coverage_command = "cargo llvm-cov -q --lcov --output-path coverage.out",
+            project_files_only = false,
+            coverage_file = "coverage.out",
+          },
+        },
+      })
       require("which-key").add({
         {
           "<leader>tc",
-          "<cmd>CoverageLoad<CR><cmd>CoverageShow<CR>",
-          desc = "Show coverage",
+          "<cmd>Coverage<CR>",
+          desc = "Create coverage report and show",
+          mode = "n",
+          icon = "󱖫",
+        },
+        {
+          "<leader>tC",
+          "<cmd>CoverageToggle<CR>",
+          desc = "Toggle coverage report on/off",
           mode = "n",
           icon = "󱖫",
         },
