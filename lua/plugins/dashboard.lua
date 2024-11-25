81be23e84ba2ff3b5c86285aa3f2101c37915a06
thejezzi <38394992+thejezzi@users.noreply.github.com>
2024-11-25 11:25:46 +0100

    update lazy-lock.json and fix dashboard.nvim config

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -101,7 +101,7 @@ return {
         header = vim.split(ascii, "\n"),
         -- stylua: ignore
         center = {
-          { action = LazyVim.telescope("files"),                                    desc = " Find File",       icon = " ", key = "f" },
+          { action = 'lua LazyVim.pick()()',                                 desc = " Find File",       icon = " ", key = "f" },
           { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
           { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
           { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
