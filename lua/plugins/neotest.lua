92f4b9e3677da5a4d718702eb3b440ea6793c076
thejezzi <38394992+thejezzi@users.noreply.github.com>
2025-02-25 01:37:53 +0100

    feat: add pretty_printer for codelldb

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -23,7 +23,22 @@ return {
         })
       end
 
-      table.insert(opts.adapters, "neotest-rust")
+      local pretty_printer_path = vim.fn.stdpath("config") .. "/pretty_printer.py"
+      -- Patch for neotest-rust: override build_spec, if dap mode is used to enable
+      -- pretty printing script
+      local rust_adapter = require("neotest-rust")
+      local orig_build_spec = rust_adapter.build_spec
+      rust_adapter.build_spec = function(args)
+        local spec = orig_build_spec(args)
+        if args.strategy == "dap" and spec.strategy and spec.strategy.type == "codelldb" then
+          spec.strategy.initCommands = {
+            "command script import " .. pretty_printer_path,
+          }
+        end
+        return spec
+      end
+
+      table.insert(opts.adapters, rust_adapter)
       table.insert(opts.adapters, "neotest-gtest")
       opts.adapters["neotest-golang"] = {
         go_test_args = {
--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -1,65 +1,106 @@
+local pretty_printer_path = vim.fn.stdpath("config") .. "/pretty_printer.py"
+local function load_dap_configuration(type)
+  -- default
+  ---@type rustaceanvim.dap.client.Config
+  local dap_config = {
+    name = "Rust debug client",
+    type = type,
+    request = "launch",
+    stopOnEntry = false,
+    initCommands = {
+      "command script import " .. pretty_printer_path,
+    },
+  }
+  if type == "lldb" then
+    ---@diagnostic disable-next-line: inject-field
+    dap_config.runInTerminal = true
+  end
+  ---@diagnostic disable-next-line: different-requires
+  local dap = require("dap")
+  -- Load configurations from a `launch.json`.
+  -- It is necessary to check for changes in the `dap.configurations` table, as
+  -- `load_launchjs` does not return anything, it loads directly into `dap.configurations`.
+  local pre_launch = vim.deepcopy(dap.configurations) or {}
+  for name, configuration_entries in pairs(dap.configurations) do
+    if pre_launch[name] == nil or not vim.deep_equal(pre_launch[name], configuration_entries) then
+      -- `configurations` are tables of `configuration` entries
+      -- use the first `configuration` that matches
+      for _, entry in pairs(configuration_entries) do
+        ---@cast entry rustaceanvim.dap.client.Config
+        if entry.type == type then
+          dap_config = entry
+          break
+        end
+      end
+    end
+  end
+  return dap_config
+end
+
 return {
   "mrcjkb/rustaceanvim",
-  opts = {
-    server = {
-      on_attach = function(_, bufnr)
-        vim.keymap.set("n", "<leader>cR", function()
-          vim.cmd.RustLsp("codeAction")
-        end, { desc = "Code Action", buffer = bufnr })
-        vim.keymap.set("n", "<leader>dr", function()
-          vim.cmd.RustLsp("debuggables")
-        end, { desc = "Rust Debuggables", buffer = bufnr })
-      end,
-      default_settings = {
-        -- rust-analyzer language server configuration
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-            loadOutDirsFromCheck = true,
-            buildScripts = {
-              enable = true,
-            },
-          },
-          -- Add clippy lints for Rust.
-          checkOnSave = {
-            allFeatures = true,
-            command = "clippy",
-            extraArgs = { "--no-deps" },
-          },
-          diagnostics = {
-            disabled = {
-              "non_snake_case",
-            },
-          },
-          procMacro = {
-            enable = true,
-            ignored = {
-              ["async-trait"] = { "async_trait" },
-              ["napi-derive"] = { "napi" },
-              ["async-recursion"] = { "async_recursion" },
-              leptos_macro = {
-                -- "component",
-                -- "server",
+  opts = function()
+    return {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust Debuggables", buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
               },
             },
-          },
-          files = {
-            excludeDirs = {
-              ".direnv",
-              ".git",
-              ".github",
-              ".gitlab",
-              "bin",
-              "node_modules",
-              "target",
-              "venv",
-              ".venv",
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            diagnostics = {
+              disabled = {
+                "non_snake_case",
+              },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+                leptos_macro = {
+                  -- "component",
+                  -- "server",
+                },
+              },
+            },
+            files = {
+              excludeDirs = {
+                ".direnv",
+                ".git",
+                ".github",
+                ".gitlab",
+                "bin",
+                "node_modules",
+                "target",
+                "venv",
+                ".venv",
+              },
             },
           },
         },
       },
-    },
-  },
+    }
+  end,
   config = function(_, opts)
     if LazyVim.has("mason.nvim") then
       local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
@@ -71,6 +112,9 @@ return {
       end
       opts.dap = {
         adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb, library_path),
+        configuration = function()
+          return load_dap_configuration("codelldb")
+        end,
       }
     end
     vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
