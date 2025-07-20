389f26319c4fbd564da31799eb34db54b11b28a8
thejezzi <38394992+thejezzi@users.noreply.github.com>
2025-07-20 15:05:26 +0200

    feat(commands): add :ChatFullScreen command
    This commit introduces a new user command `:ChatFullScreen` that opens
    the CodeCompanion chat interface in a full-screen window.

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -5,3 +5,12 @@
 vim.api.nvim_create_user_command("CustomChangeFileType", function()
   require("lib").change_filetype_window()
 end, {})
+
+-- Erstellt deinen neuen Benutzerbefehl :ChatFullScreen
+vim.api.nvim_create_user_command("ChatFullScreen", function()
+  require("codecompanion").chat({
+    use_system_prompt = false,
+  })
+
+  vim.cmd("only")
+end, { nargs = 0 })
